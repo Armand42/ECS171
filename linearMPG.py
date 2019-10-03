a62aae8aa41b47ca244da4e6595ac6729fcd2b29
Armand Nasseri <aanasseri@ucdavis.edu>
2019-10-03 15:40:37 -0700

    Problem 2 perfect

--- linearMPG.py
+++ linearMPG.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from pandas.plotting import scatter_matrix
+from numpy.linalg import inv
 # drop Na to specify rows with question marks!!
 dataset = pd.read_csv('auto-mpg.data', delim_whitespace=True)
 
@@ -53,18 +54,67 @@ dataset.loc[np.logical_and(mpg > med, mpg < high), 'threshold']= "high"
 # very high
 dataset.loc[mpg >= high, 'threshold'] = "very high" 
 
-sns.lmplot(x ="Displacement",y="Weight", data=dataset, fit_reg=False, hue='threshold', legend=False)
+#sns.lmplot(x ="Displacement",y="Weight", data=dataset, fit_reg=False, hue='threshold', legend=False)
+#sns.lmplot(x ="Weight",y="Horsepower", data=dataset, fit_reg=False, hue='threshold', legend=False)
+#plt.legend(loc='lower right')
+# Option 1
+# Actual plot with all variables against each other
+#scatter_matrix(dataset.loc[:,'Cylinder':'Origin'], alpha=0.2, figsize=(20, 20))
 
+# Option 2
+sns.pairplot(dataset.loc[:, dataset.columns != 'mpg'], hue='threshold')
 plt.legend(loc='lower right')
 
-# Actual plot with all variables against each other
-scatter_matrix(dataset.loc[:,'Cylinder':'Origin'], alpha=0.2, figsize=(20, 20))
+
+
+# Problem 3
+# test works
+xxx = np.array([[1,1,1,1,1], [49,69,89,99,109]])
+yyyy = np.array([124,95,71,45,18])
+# Transpose goes into second argument
+X = np.transpose(xxx)
+# CAREFUL WITH INPUT
+exp1 = np.dot(xxx,X)
+expB = np.dot(yyyy,X)
+f = inv(exp1)
+finalRes = np.dot(f,expB)
 
 
 
+
+
+dataset.insert(10, 'bias', 1)
+bias= dataset.iloc[:,10].values
+
+
+
+def linearReg(x,y):
+    # Add ones to x
+    combinedX = np.column_stack((bias, x))
+    # Transpose x
+    Xtransposed = np.transpose(combinedX)
+    # First expression
+    A = np.dot(combinedX,Xtransposed)
+    # Second expression
+    # SOMETHING WRONG HERE
+    B = np.dot(Xtransposed,y)
+    # Inverse 
+    #inverse = inv(A)
+    
+    # result
+    #result = np.dot(inverse,B)
+    
+    #print(A.shape, A.ndim)
+    print(B.shape, B.ndim)
+    return A
+    
+# x and y
+testReg = linearReg(wt,mpg)
+print(linearReg(wt,mpg))
    
-
-
+#z = np.ones((392,1))
+#combined= np.column_stack((z, wt))
+#print(combined)
 
 
 # sample data weight vs mpg
